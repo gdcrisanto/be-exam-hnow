95a5cd4003954279c6dc3d092705927ffa673744
Diego <gdcrisanto@gmail.com>
2023-03-11 05:28:08 +0800

    integrate handlers

--- router/user.ts
+++ router/user.ts
@@ -1,11 +1,16 @@
 import { Router } from "express";
-import { createUser } from "../handlers/userHandlers";
+import {
+  createUser,
+  deleteUser,
+  getUsers,
+  updateUser,
+} from "../handlers/userHandlers";
 
 const userRouter = Router();
 
 userRouter.post("/", createUser);
-userRouter.patch("/");
-userRouter.delete("/");
-userRouter.get("/");
+userRouter.patch("/", updateUser);
+userRouter.delete("/", deleteUser);
+userRouter.get("/", getUsers);
 
 export default userRouter;
