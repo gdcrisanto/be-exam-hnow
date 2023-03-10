cb62b7270e8d2420d5b4f95ad442c9a9fde5acdb
Diego <gdcrisanto@gmail.com>
2023-03-11 06:14:44 +0800

    fix env variables

--- server.ts
+++ server.ts
@@ -1,7 +1,13 @@
 import { Sequelize } from "sequelize";
+require("dotenv").config({ path: ".env" });
 
-const sequelize = new Sequelize("test", "root", "root", {
-  host: "localhost",
+const db = process.env.DB || "db";
+const user = process.env.USERNAME || "user";
+const pass = process.env.PASS || "pass";
+const host = process.env.HOST || "localhost";
+
+const sequelize = new Sequelize(db, user, pass, {
+  host: host,
   dialect: "mysql",
   port: 3307,
 });
